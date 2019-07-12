fe51801d3a7034a3be3d7d8bfb861dcc247f877e
Adil El Bourki <adilelbourki@gmail.com>
2019-07-12 10:23:40 +0200

    création du document

---
+++ src/main/java/ma/elbourki/ged/infosat/GestionDesDocumentInfosatApplication.java
@@ -0,0 +1,13 @@
+package ma.elbourki.ged.infosat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDesDocumentInfosatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDesDocumentInfosatApplication.class, args);
+	}
+
+}
